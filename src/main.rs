0d3ecd7a5eefd4e198a4295a10befb1a94baa6bc
Wink Saville <wink@saville.com>
2023-10-08 13:06:32 -0700

    feat: Add predict and mean_squared_error to actually use the cofficients
    Also added a test and use a seeded PRNG so consistent results.

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,6 @@
-extern crate rand;
-extern crate nalgebra as na;
-
-use rand::Rng;
-
-const NUM_POINTS: usize = 10;
+use nalgebra as na;
+use rand::rngs::StdRng;
+use rand::{Rng, SeedableRng};
 
 struct Point3D {
     x: f64,
@@ -11,8 +8,8 @@ struct Point3D {
     z: f64,
 }
 
-fn generate_random_points(num_points: usize) -> Vec<Point3D> {
-    let mut rng = rand::thread_rng();
+fn generate_random_points(num_points: usize, seed: u64) -> Vec<Point3D> {
+    let mut rng = StdRng::seed_from_u64(seed);
     let mut points = Vec::new();
 
     for _ in 0..num_points {
@@ -25,7 +22,6 @@ fn generate_random_points(num_points: usize) -> Vec<Point3D> {
 
     points
 }
-
 
 fn polynomial_regression(points: &[Point3D]) -> na::DVector<f64> {
     let n = points.len();
@@ -46,15 +42,64 @@ fn polynomial_regression(points: &[Point3D]) -> na::DVector<f64> {
         y[i] = point.z;
     }
 
+    // Return cofficients, not sure why we need to transpose
     let xt = x.transpose();
-    let coefficients = (xt.clone() * x).try_inverse().unwrap() * xt * y;
+    (xt.clone() * x).try_inverse().unwrap() * xt * y
+}
 
-    coefficients
+fn predict(point: &Point3D, coeffs: &na::DVector<f64>) -> f64 {
+    coeffs[0] * point.x.powi(3)
+        + coeffs[1] * point.x.powi(2) * point.y
+        + coeffs[2] * point.x * point.y.powi(2)
+        + coeffs[3] * point.y.powi(3)
+        + coeffs[4] * point.x.powi(2)
+        + coeffs[5] * point.x * point.y
+        + coeffs[6] * point.y.powi(2)
+        + coeffs[7] * point.x
+        + coeffs[8] * point.y
+        + coeffs[9]
+}
+
+fn mean_squared_error(points: &[Point3D], coeffs: &na::DVector<f64>) -> f64 {
+    let n = points.len() as f64;
+    points
+        .iter()
+        .map(|point| (point.z - predict(point, coeffs)).powi(2))
+        .sum::<f64>()
+        / n
 }
 
 fn main() {
-    let data = generate_random_points(NUM_POINTS);
+    const NUM_POINTS: usize = 10;
+    const SEED: u64 = 12345678;
+
+    let data = generate_random_points(NUM_POINTS, SEED);
 
     let coeffs = polynomial_regression(&data);
-    println!("{:?}", coeffs);
+    println!("coeffs: {:?}", coeffs);
+
+    let mse = mean_squared_error(&data, &coeffs);
+    println!("mse: {mse}");
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_regression() {
+        const NUM_POINTS: usize = 10;
+        const SEED: u64 = 987654321;
+
+        let data = generate_random_points(NUM_POINTS, SEED);
+
+        let coeffs = polynomial_regression(&data);
+
+        // A simple test might be to ensure the MSE is below a certain threshold
+        let mse = mean_squared_error(&data, &coeffs);
+        println!("test_regression: mse={mse}");
+        assert!(mse < 1.0); // Adjust this threshold as needed
+
+        // Add more specific tests as needed
+    }
 }
